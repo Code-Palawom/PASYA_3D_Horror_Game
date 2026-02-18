8abf9531c128a36eb34b5e01058fa3fbe37dec90
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-02-19 01:42:52 +0800

    Added neccessary movements
    e.g jumping, sprinting, and crouching

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
-    [SerializeField] private Transform orientation;
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Transform cameraOrientation;
+    [SerializeField] private float baseMoveSpeed = 5f;
+    [SerializeField] private float crouchSpeed = 2f;
+    [SerializeField] private float sprintSpeed = 9f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private CinemachineCamera firstPersonPOV;
     [SerializeField] private CinemachineCamera thirdPersonPOV;
@@ -20,9 +22,11 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     private Vector2 moveInput;
+    private float moveSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
+        moveSpeed = baseMoveSpeed;
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
@@ -34,9 +38,35 @@ public class PlayerController : MonoBehaviour {
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void OnCrouch(InputAction.CallbackContext context) {
+        if(context.started) {
+            moveSpeed = crouchSpeed;
+            Debug.Log("Crouching");
+        }
+
+        if(context.canceled) {
+            moveSpeed = baseMoveSpeed;
+            Debug.Log("Not crouching");
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context) {
+        if(context.started) {
+            moveSpeed = sprintSpeed;
+            Debug.Log("Sprinting");
+        }
+
+        if(context.canceled) {
+            moveSpeed = baseMoveSpeed;
+            Debug.Log("Not sprinting");
+        }
+    }
+
     public void OnJump(InputAction.CallbackContext context) {
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         AudioSource.PlayOneShot(jumpSound);
+
+        Debug.Log("Jumped");
     }
 
     public void OnSwitchPOV(InputAction.CallbackContext context) {
@@ -49,13 +79,15 @@ public class PlayerController : MonoBehaviour {
             firstPersonPOV.Priority = 0;
             thirdPersonPOV.Priority = 10;
         }
+
+        Debug.Log($"POV Switched; is First Person {isFirstPersonPOV}");
     }
 
     // Update is called once per frame
     void Update() {
         // Movements for First Person POV
         if(isFirstPersonPOV) {
-            Vector3 cameraForward = orientation.forward;
+            Vector3 cameraForward = cameraOrientation.forward;
             cameraForward.y = 0f;
 
             if(cameraForward != Vector3.zero) {
@@ -71,15 +103,15 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(move.normalized * moveSpeed * 10f, ForceMode.Force);
 
         if(!isFirstPersonPOV && move.sqrMagnitude > 0.001f) {
-            Vector3 cameraForward = orientation.forward;
+            Vector3 cameraForward = cameraOrientation.forward;
             cameraForward.y = 0f;
 
             Quaternion newRotation = Quaternion.LookRotation(cameraForward);
             rb.MoveRotation(Quaternion.Slerp(transform.rotation, newRotation, 10f * Time.deltaTime));
 
             // Borkoloy nga move
-            //Vector3 forward = orientation.forward;
-            //Vector3 right = orientation.right;
+            //Vector3 forward = cameraOrientation.forward;
+            //Vector3 right = cameraOrientation.right;
 
             //forward.y = 0;
             //right.y = 0;
