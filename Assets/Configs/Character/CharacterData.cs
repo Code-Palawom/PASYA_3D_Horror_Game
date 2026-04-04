dcc0914ae628dfdb8fd58879ae5aa3303426520d
Alex <130986014+AylexCODE@users.noreply.github.com>
2026-04-04 17:41:23 +0800

    Added initial cosmetics
    New watch cosmetic

--- Assets/Configs/Character/CharacterData.cs
+++ Assets/Configs/Character/CharacterData.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class CharacterData : ScriptableObject {
     //int health = 100;
     [Header("Mesh")]
+    public Mesh cosmeticMesh;
     public Mesh hairMesh;
     public Mesh bodyMesh;
     
     [Header("Material")]
     public Material hairMaterial;
     public Material bodyMaterial;
+    public Material[] cosmeticMaterial;
     //public RuntimeAnimatorController animatorController;
 }
--- Assets/Player/Scripts/Character/ChangeCharacterHandler.cs
+++ Assets/Player/Scripts/Character/ChangeCharacterHandler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ChangeCharacterHandler : MonoBehaviour {
     [Header("Renderers")]
+    public SkinnedMeshRenderer cosmeticRenderer;
     public SkinnedMeshRenderer hairRenderer;
     public SkinnedMeshRenderer bodyRenderer;
 
@@ -13,6 +14,7 @@ public class ChangeCharacterHandler : MonoBehaviour {
 
     public void ApplyCharacter(CharacterData currentData) {
         if(currentData == null) return;
+        ChangeCosmetic(cosmeticRenderer, currentData.cosmeticMesh, currentData.cosmeticMaterial);
 
         UpdatePart(hairRenderer, currentData.hairMesh, currentData.hairMaterial);
         UpdatePart(bodyRenderer, currentData.bodyMesh, currentData.bodyMaterial);
@@ -31,5 +33,18 @@ public class ChangeCharacterHandler : MonoBehaviour {
         renderer.gameObject.SetActive(true);
         renderer.sharedMesh = mesh;
         renderer.sharedMaterial = material;
+    }
+
+    private void ChangeCosmetic(SkinnedMeshRenderer renderer, Mesh mesh, Material[] material) {
+        if(renderer == null) return;
+
+        if(mesh == null){
+            renderer.gameObject.SetActive(false);
+            return;
+        }
+
+        renderer.gameObject.SetActive(true);
+        renderer.sharedMesh = mesh;
+        renderer.sharedMaterials = material;
     }
 }
